a60b71cf94bdde23ac7972da00f25767f24c3839
Paul BIE <pbie@student.42.fr>
2018-03-23 14:26:58 +0100

    [Add] - Into

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+	constructor (props) {
+		super(props)
+
+		this.onSubmit = this.onSubmit.bind(this)
+		this.onClick = this.onClick.bind(this)
+
+		this.state = {
+			title: 'App Title'
+		}
+	}
+
+	onClick () {
+		this.setState({
+			title: 'New app title'
+		})
+	}
+
+	onChange (event) {
+		console.log(event.target.value)
+	}
+
+	onSubmit (event) {
+		event.preventDefault()
+		console.log(`this.input.value`, this.input.value)
+	}
+
+	render() {
+		const list = [ 'item1', 'item2', 'item3' ]
+		return (
+			<div className="App">
+				<h1>{ this.state.title }</h1>
+				<div onClick={this.onClick}></div>
+				<h1>
+					{
+						list.map((item, index) => {
+							return (
+								<div onClick={ this.onClick } key={ index }>{ item }</div>
+							)
+						})
+					}
+				</h1>
+				<form onSubmit={ this.onSubmit } action="">
+					<input type="text" onChange={ this.onChange } ref={ input => this.input = input } />
+				</form>
+			</div>
+		)
+	}
 }
 
 export default App;
